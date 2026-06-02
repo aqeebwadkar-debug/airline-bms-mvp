d38446b4d95878fec5a2dd38a718ca64b6233b89
aqeebwadkar-debug <aqeeb.wadkar@alphaobs.com>
2026-06-02 18:59:29 +0530

    Added chatbot route metadata

---
+++ app/passenger-chatbot/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "BMS Passenger Chatbot",
+};
+
+export default function PassengerChatbotLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
